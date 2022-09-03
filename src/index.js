13d50234ff0da6b9038d903166b036357c1c7287
Mikhaylo Pobochikh <pma.ua01@gmail.com>
2022-09-03 13:15:00 +0300

    end

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import { renderCountryMore, renderCountryOne } from './render';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import Handlebars from 'handlebars';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -7,56 +12,36 @@ const refs = {
   countryList: document.querySelector('.country-list'),
 };
 
-console.log(refs.input);
-
-refs.input.addEventListener('input', onInput);
+refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(e) {
-  console.log('that', e.target.value);
-  fetchCountries(`${e.target.value}`).then(r => renderCountry(r));
-}
+  e.target.value = `${e.target.value}`.trim();
+  if (e.target.value === '') {
+    return (refs.countryList.innerHTML = '');
+  }
 
-function fetchCountries(name) {
-  return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages `
-  ).then(r => {
-    return r.json();
-  });
+  fetchCountries(`${e.target.value}`)
+    .then(r => renderCountry(r))
+    .catch(onError);
 }
 
 function renderCountry(country) {
   if (country.length > 10) {
-    return alert('Too many matches found. Please enter a more specific name.');
+    return Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
   }
   if (country.length > 2 || country.length <= 10) {
-    const markup = country
-      .map(
-        ({ flags, name, capital, population }) =>
-          `<li class="county-search"><img src="${flags.svg}" class="flag"><p>${name.official}</p></li>`
-      )
-      .join('');
-    refs.countryList.innerHTML = markup;
+    refs.countryList.innerHTML = renderCountryMore(country);
   }
   if (country.length === 1) {
-    const markup = country
-      .map(
-        ({ flags, name, capital, population, languages }) =>
-          `<li class=""><img src="${flags.svg}" class="flag"><p>${
-            name.official
-          }</p><div class="country-one"><p>Capital:${capital}</p><p>Population:${population}</p><p>Languages:${Object.values(
-            languages
-          )}</p></div></li>`
-      )
-      .join('');
-    refs.countryList.innerHTML = markup;
+    refs.countryList.innerHTML = renderCountryOne(country);
   }
 }
 
-// const r = fetch(
-//   `https://restcountries.com/v3.1/name/sw?fields=name,capital,population ,flags,languages `
-// )
-//   .then(r => r.json())
-//   .then(obj => console.log(obj));
-// console.log(r);
+function onError(error) {
+  refs.countryList.innerHTML = '';
+  Notiflix.Notify.failure('Oops, there is no country with that name');
+}
 
-fetchCountries('ua').then(r => console.log(r));
+fetchCountries('sw').then(r => console.log(r));
